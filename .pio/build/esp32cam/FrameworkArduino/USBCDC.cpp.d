e3b4945cbf7380ed40bb333fdf9690d3853f932a
Andrei Manz <andrei.manz@gmail.com>
2025-09-10 19:02:19 +0300

    Clean up repository - remove build files and add .gitignore

--- .pio/build/esp32cam/FrameworkArduino/USBCDC.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32cam/FrameworkArduino/USBCDC.cpp.o: \
- /Users/andrei/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBCDC.cpp \
- /Users/andrei/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
- /Users/andrei/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/qio_qspi/include/sdkconfig.h
--- .pio/build/esp32cam/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32cam/FrameworkArduino/cbuf.cpp.o: \
- /Users/andrei/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- /Users/andrei/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
